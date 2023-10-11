e5f69680485513e8bdf9cf7f451ce83af41fe463
blu3id <blu3id@users.noreply.github.com>
2023-10-12 00:08:55 +0100

    Temp dir ACL Workarround

--- .github/workflows/update.ps1
+++ .github/workflows/update.ps1
@@ -52,6 +52,19 @@ function Extract-Updates {
 Write-Host "> Installing Windows Feature `"UpdateServices`" - WSUS Role"
 Install-WindowsFeature -Name UpdateServices -IncludeManagementTools
 
+Write-Host "> Setting FullControl ACL for `"NT AUTHORITY\NetworkService`" on `"C:\Windows\temp`""
+$NewAcl = Get-Acl -Path "C:\Windows\Temp"
+# Set properties
+$identity = "NT AUTHORITY\NetworkService"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+# Create new rule
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+# Apply new rule
+$NewAcl.SetAccessRule($fileSystemAccessRule)
+Set-Acl -Path "C:\Windows\Temp" -AclObject $NewAcl
+
 Write-Host "> Running WSUS Post-install"
 $WSUSDir = "C:\WSUS_Updates"
 C:\Program` Files\Update` Services\Tools\wsusutil.exe postinstall CONTENT_DIR=$WSUSDir
